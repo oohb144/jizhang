a428d4598273f2c138024b1628260ba51f7f759a
wanboxiang <2827878731@qq.com>
2026-09-05 23:16:23 +0800

    feat: process auto captures on launch and resume

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'database/v2/data_store_bootstrap.dart';
 import 'screens/home_screen.dart';
 import 'services/app_runtime.dart';
+import 'services/auto_capture/auto_capture_coordinator.dart';
 
 DataStoreBootstrapResult? v2DataStore;
 
@@ -41,7 +42,60 @@ class MyApp extends StatelessWidget {
           elevation: 0,
         ),
       ),
-      home: const HomeScreen(),
+      home: const AutoCaptureAwareHome(),
     );
+  }
+}
+
+class AutoCaptureAwareHome extends StatefulWidget {
+  const AutoCaptureAwareHome({super.key});
+
+  @override
+  State<AutoCaptureAwareHome> createState() => _AutoCaptureAwareHomeState();
+}
+
+class _AutoCaptureAwareHomeState extends State<AutoCaptureAwareHome>
+    with WidgetsBindingObserver {
+  int _revision = 0;
+  bool _processing = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    WidgetsBinding.instance.addPostFrameCallback((_) => _processCaptures());
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      _processCaptures();
+    }
+  }
+
+  Future<void> _processCaptures() async {
+    if (_processing) return;
+    _processing = true;
+    try {
+      final posted = await AutoCaptureCoordinator(
+        pendingDatabase: AppRuntime.v2Database,
+      ).processPendingNativeCaptures();
+      if (posted > 0 && mounted) {
+        setState(() => _revision++);
+      }
+    } finally {
+      _processing = false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return HomeScreen(key: ValueKey(_revision));
   }
 }
